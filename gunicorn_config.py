4908fc288b635e16af3e335255d0eee89c6f56d2
benoitl <lambert.bnt@gmail.com>
2024-02-18 13:00:29 +0100

    Added gunicorn config

---
+++ gunicorn_config.py
@@ -0,0 +1,11 @@
+import os
+
+loglevel = 'info'
+logfile = 'logs/gunicorn_logfile.log'
+accesslog = 'logs/gunicorn_accesslog.log'
+errorlog = 'logs/gunicorn_errorlog.log'
+
+access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+bind = "localhost:" + str(os.environ.get('DEPLOY_PORT', 8080))
+workers = 3
