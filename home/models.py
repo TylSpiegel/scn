16d21cc835db36ac5fa196d15c302661c603710a
benoitl <lambert.bnt@gmail.com>
2024-04-14 11:00:53 +0200

    POC for generic styling => Added a model with env variable for styling Updated html and css to be able to use those values -- The exact values are prototypical at this point => primary color / secondary color / logo This has to be improved.

--- home/models.py
+++ home/models.py
@@ -5,29 +5,58 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
 from wagtail.blocks import RichTextBlock, StreamBlock
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
+from wagtail.admin.panels import FieldPanel
+
+from wagtail_color_panel.fields import ColorField
+from wagtail_color_panel.edit_handlers import NativeColorPanel
 
 from .blocks import ButtonBlock, ImageBlock, MultiColumnBlock
 
 
 class HomePage(Page):
-	name = TextField(help_text="Titre du site")
-	header = TextField(help_text="En-tête")
-	body = RichTextField(help_text="Contenu de la page")
+    name = TextField(help_text="Titre du site")
+    header = TextField(help_text="En-tête")
+    body = RichTextField(help_text="Contenu de la page")
 
-	content_panels = Page.content_panels + [
-		FieldPanel('name'),
-		FieldPanel('header'),
-		FieldPanel('body'),
-	]
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        FieldPanel('header'),
+        FieldPanel('body'),
+    ]
 
 
 class ContentPage(Page):
-	content = StreamField([
-		('contenu', RichTextBlock()),
-		('image', ImageBlock()),
-		('colonnes', MultiColumnBlock())
-	], null=True, blank=True, use_json_field=True)
+    content = StreamField([
+        ('contenu', RichTextBlock()),
+        ('image', ImageBlock()),
+        ('colonnes', MultiColumnBlock())
+    ], null=True, blank=True, use_json_field=True)
 
-	content_panels = Page.content_panels + [
-		FieldPanel("content"),
-	]
+    content_panels = Page.content_panels + [
+        FieldPanel("content"),
+    ]
+
+
+@register_setting
+class StyleSettings(BaseGenericSetting):
+    primary_color = ColorField(default='#FF0000')
+    secondary_color = ColorField(default='#FF0000')
+    logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Logo de votre site"
+    )
+
+    panels = [
+        NativeColorPanel('primary_color'),
+        NativeColorPanel('secondary_color'),
+        FieldPanel('logo'),
+    ]
+
+    class Meta:
+        verbose_name = 'Customisation'
