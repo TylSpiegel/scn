232a0ce7874e39a48b016678866a639628a36065
benoitl <lambert.bnt@gmail.com>
2024-04-14 11:34:38 +0200

    Updated default port to deploy

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,5 +7,5 @@ errorlog = 'logs/gunicorn_errorlog.log'
 
 access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
-bind = "localhost:" + str(os.environ.get('DEPLOY_PORT', 8080))
+bind = "localhost:" + str(os.environ.get('DEPLOY_PORT', 9200))
 workers = 3
