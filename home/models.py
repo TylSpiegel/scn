1fd55d2218a1102ba3b3a4f38dcc1025d4bd2b44
benoitl <lambert.bnt@gmail.com>
2024-05-01 22:14:39 +0200

    Added fields in customisation menus to customize message_password text and image. Changed password_required page to be clearer, nicer and responsive

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ from wagtail.models import Page
 from wagtail.blocks import RichTextBlock, StreamBlock
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, TabbedInterface, ObjectList
 
 from wagtail_color_panel.fields import ColorField
 from wagtail_color_panel.edit_handlers import NativeColorPanel
@@ -41,9 +41,19 @@ class ContentPage(Page):
 
 @register_setting
 class StyleSettings(BaseGenericSetting):
+    # THEME
     primary_color = ColorField(default='#FF0000')
     secondary_color = ColorField(default='#FF0000')
     third_color = ColorField(default='#FF0000')
+
+    generic_theme = models.BooleanField(default=False)
+
+    theme_panel = [
+        FieldPanel('primary_color'),
+        FieldPanel('secondary_color'),
+        FieldPanel('third_color'),
+    ]
+    # BRANDING
     logo = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -52,15 +62,40 @@ class StyleSettings(BaseGenericSetting):
         related_name='+',
         help_text="Logo de votre site"
     )
-    generic_theme = models.BooleanField(default=False)
 
-    panels = [
-        NativeColorPanel('primary_color'),
-        NativeColorPanel('secondary_color'),
-        NativeColorPanel('third_color'),
+    branding_panel = [
         FieldPanel('logo'),
         FieldPanel('generic_theme', help_text='Thème générique:')
     ]
+
+    # PASSWORD
+
+    password_message = RichTextField(
+        help_text="Le texte qui sera affiché sur la page de demande de mot de passe.",
+        default="",
+    )
+
+    image_password = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Image qui sera affichée sur la page de demande de mot de passe."
+    )
+
+    password_panel = [
+        FieldPanel('password_message'),
+        FieldPanel('image_password')
+    ]
+
+    #####
+
+    edit_handler = TabbedInterface([
+        ObjectList(theme_panel, heading='Thème'),
+        ObjectList(branding_panel, heading='Branding'),
+        ObjectList(password_panel, heading='Password'),
+    ])
 
     class Meta:
         verbose_name = 'Customisation'
