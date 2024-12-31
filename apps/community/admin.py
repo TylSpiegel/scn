7ad97a338dcbf428003d4b76f9b9bddaa6e5a66b
Camille Dasseleer <camille.dasseleer@uclouvain.be>
2024-12-31 18:35:19 +0100

    Some more refactoring

--- apps/community/admin.py
+++ apps/community/admin.py
@@ -1,5 +1,5 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
-from .models.repertoire import Choriste, Evenement
+from ..music.models.repertoire import Choriste, Evenement
 from django.contrib import admin
 
 
