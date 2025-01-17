c751b0a2e26b8dedcb89d903d8503ba6b65b5716
benoitl <lambert.bnt@gmail.com>
2025-01-17 22:42:56 +0100

    Updated core config files

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,35 +1,48 @@
+"""URL Configuration for website project."""
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.images import urls as wagtailimages_urls
 
 from search import views as search_views
 
 urlpatterns = [
+    # Django Admin
     path("django-admin/", admin.site.urls),
+
+    # Wagtail Admin
     path("admin/", include(wagtailadmin_urls)),
+
+    # Wagtail documents and images
     path("documents/", include(wagtaildocs_urls)),
-    path("search/", search_views.search, name="search"),
+    path("images/", include(wagtailimages_urls)),
+
+    # Custom app URLs
+    path("music/", include("apps.music.urls")),
+    path("community/", include("apps.community.urls")),
+
+    # For anything not caught by a more specific rule above, hand over to
+    # Wagtail's serving mechanism
+    path("", include(wagtail_urls)),
 ]
 
-
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
-]
+    # Django Debug Toolbar
+    try:
+        import debug_toolbar
+
+        urlpatterns = [
+                          path('__debug__/', include(debug_toolbar.urls)),
+                      ] + urlpatterns
+    except ImportError:
+        pass
--- apps/core/wsgi.py
+++ apps/core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for scn_website project.
+WSGI config for website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
+# Set the Django settings module path
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apps.core.settings.dev")
 
 application = get_wsgi_application()
